ace0d929ed6b99491a05c708a8d0dbe0e6c37203
Aneesh <aneeshy1508cse.com>
2026-02-18 11:46:52 +0530

    add Speciality

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,10 +6,13 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-         'primary':"#5f6FFF"
+      colors: {
+        'primary': "#5f6FFF"
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
       }
-     
+
     },
   },
   plugins: [],
