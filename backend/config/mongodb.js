463289c412b3c663a46e70079d7fb39260bf4092
Aneesh Yadav <aneeshy1508cse@gmail.com>
2026-02-27 17:13:55 +0530

    add backend

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,5 +1,9 @@
+import dns from "node:dns/promises";
+// Current DNS check
+console.log(await dns.getServers()); // Agar 127.0.0.53 dikhe, change karo
+// Set reliable DNS
+dns.setServers(["1.1.1.1"]); // Cloudflare DNS
 import mongoose from "mongoose";
-
 const connectDB = async () => {
 
     mongoose.connection.on('connected',() => console.log("Databas Connected"))
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
+
 import express from 'express'
+import dotenv from 'dotenv';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+
+dotenv.config();
+
 
 //app config
+
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
@@ -15,6 +22,9 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
+
+app.use('/api/admin',adminRouter)
+
 
 app.get('/', (req,res) => {
     res.send("API Working egt")
