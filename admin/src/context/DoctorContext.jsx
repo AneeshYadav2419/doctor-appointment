17023fd53d73a564574777ddc44a6111c7e95939
Aneesh Yadav <aneeshy1508cse@gmail.com>
2026-03-08 19:05:51 +0530

    add admin panel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import {toast} from 'react-toastify'
 import { createContext } from "react";
-
+import axios from 'axios'
 export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
-    const value = {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken'):'')
 
+    const [appointments,setAppointments] = useState([])
+    const getAllAppointments =async () =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/appointments',{headers:{dToken}})
+            if(data.success){
+                setAppointments(data.appointments.reverse())
+                console.log(data.appointments.reverse())
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+          console.log(error)
+          toast.error(error.message)
+       // res.json({success:false, message:error.message})
+        }
+    }
+    const value = {
+        dToken, setDToken,
+        backendUrl,
+        appointments,setAppointments,getAllAppointments
     }
     return(
         <DoctorContext.Provider value={value}>
