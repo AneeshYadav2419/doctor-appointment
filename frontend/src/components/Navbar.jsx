ee864610757b8d28d35453d273c0ebf45f547992
Aneesh Yadav <aneeshy1508cse@gmail.com>
2026-03-10 15:51:36 +0530

    add new Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+// import { assets } from "../assets/assets";
 import { NavLink, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
@@ -15,8 +16,10 @@ const Navbar = () => {
     }
     return (
 
-        <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <div className='flex items-center justify-between text-sm py-2 mb-5 border-b border-b-gray-400'>
             <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt='' />
+
+
             <ul className='hidden md:flex items-start gap-5 font-medium'>
                 <NavLink to="/">
                     <li className='py-1'>HOME</li>
